7b98ca676ac570ab3fdc948f3b547ad590990c50
thekovic <72971433+thekovic@users.noreply.github.com>
2026-07-16 22:40:42 +0200

    repo: Remove files related to mobile versions, Docker, unused tools, etc.

--- desktop_version/fixupMac.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# flibit has this solely for convenience when cleaning up Mac binaries.
-
-install_name_tool -change /usr/local/lib/libSDL2-2.0.0.dylib @rpath/libSDL2-2.0.0.dylib VVVVVV 
-strip -S VVVVVV
-otool -L VVVVVV
